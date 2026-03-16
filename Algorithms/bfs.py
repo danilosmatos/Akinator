5f7fc894d243cd6664625b3dcd3eac665a0369d1
Antonio Gabriel <antoniothecode@gmail.com>
2026-03-16 08:42:36 -0300

    otimizando bfs e subindo cpidigo de arvore

--- Algorithms/bfs.py
+++ Algorithms/bfs.py
@@ -1,67 +1,46 @@
-﻿arvoreTeste = {
-        '1':  ['2', '3', '4'],
-    '2':  ['1', '5', '6', '7'],
-    '3':  ['1', '8', '9'],
-    '4':  ['1', '10', '11', '12'],
-    '5':  ['2', '13', '14'],
-    '6':  ['2', '15'],
-    '7':  ['2', '16', '17'],
-    '8':  ['3', '18', '19'],
-    '9':  ['3', '20'],
-    '10': ['4', '21', '22'],
-    '11': ['4', '23'],
-    '12': ['4', '24', '25'],
-    '13': ['5'],
-    '14': ['5'],
-    '15': ['6'],
-    '16': ['7'],
-    '17': ['7'],
-    '18': ['8'],
-    '19': ['8'],
-    '20': ['9'],
-    '21': ['10'],
-    '22': ['10'],
-    '23': ['11'],
-    '24': ['12'],
-    '25': ['12'],
+﻿from tree import Tree
+import random
 
-}
+arvoreTeste = Tree()
+valores = random.sample(range(1, 100), 10)
+print(f'Valores da Árvore: {valores}')
+for v in valores:
+    arvoreTeste.inserir(v)
+
+grafo = arvoreTeste.para_grafo()
+print(f'Grafo da Árvore: {grafo}')
 
 def bfs(grafo, no1):
     visitados = []
     queue = [no1]
     pais = {}
-
     while queue:
         noAtual = queue.pop(0)
         visitados.append(noAtual)
-
         for vizinho in grafo[noAtual]:
             if vizinho not in visitados:
                 queue.append(vizinho)
                 pais[vizinho] = noAtual
-
     return pais
 
 def menorCaminho(pais, origem, destino):
     trilha = [destino]
     noAtual = destino
-
     while noAtual != origem:
         noAtual = pais[noAtual]
         trilha.append(noAtual)
-
     trilha.reverse()
     return trilha
-def caminho (grafo, origem, destino ):
+
+def caminho(grafo, origem, destino):
     pais = bfs(grafo, origem)
-    
     return menorCaminho(pais, origem, destino)
 
+nos = list(grafo.keys())
+raiz = str(arvoreTeste.raiz.valor)
 
-print(caminho(arvoreTeste, '1', '25'))
-print(caminho(arvoreTeste, '2', '25'))
-print(caminho(arvoreTeste, '3', '13'))
-print(caminho(arvoreTeste, '4', '13'))
+print(f'\nCaminho de {raiz} até {nos[-1]}:')
+print(caminho(grafo, raiz, nos[-1]))
 
-
+print(f'\nCaminho de {raiz} até {nos[-2]}:')
+print(caminho(grafo, raiz, nos[-2]))
--- Algorithms/tree.py
+++ Algorithms/tree.py
@@ -1,1 +1,144 @@
-﻿
+﻿import random
+class Nodes:
+    def __init__(self, valor):
+        self.valor = valor
+        self.esquerda = None
+        self.direita = None
+
+
+class Tree:
+    def __init__(self):
+        self.raiz = None
+
+    def inserir(self, dado):
+        novo = Nodes(dado)
+
+        if self.raiz == None:
+            self.raiz = novo
+            return
+        atual = self.raiz
+        while True:
+            if dado < atual.valor:
+                if atual.esquerda is None:
+                    atual.esquerda = novo
+                return
+                atual = atual.esquerda
+            else:
+                if atual.direita is None:
+                    atual.direita = novo
+                    return
+                atual = atual.direita
+    def remover(self, valor):
+        pai = None
+        atual = self.raiz
+        while atual and atual.valor != valor:
+            pai = atual
+            if valor < atual.valor:
+                atual = atual.esquerda
+            else:
+                atual = atual.direita
+
+        if not atual:
+
+            print('Elemento não encontrado.')
+            return False
+        
+        if atual.esquerda and atual.direita:
+            pai_sucessor = atual
+            sucessor = atual.direita
+
+            while sucessor.esquerda:
+                pai_sucessor = sucessor
+                sucessor = sucessor.esquerda
+
+            atual.valor = sucessor.valor
+            pai = pai_sucessor
+            atual = sucessor
+
+        filho = atual.esquerda or atual.direita
+        if pai is None:
+            self.raiz = filho
+        elif pai.esquerda == atual:
+            pai.esquerda = filho
+        else:
+            pai.direita = filho
+        return True
+
+
+    def buscar(self, valor):
+
+        atual = self.raiz
+        while atual:
+            if valor == atual.valor:
+                print(f'Valor encontrado: {valor}')
+                return True
+            if valor < atual.valor:
+                atual = atual.esquerda
+
+            else:
+                atual = atual.direita
+        print('Valor não encontrado!')
+    def altura(self, no):
+        if no is None:
+            return 0
+        alt_esquerda = self.altura(no.esquerda)
+        alt_direita = self.altura(no.direita)
+        return 1 + max(alt_esquerda, alt_direita)
+    
+    def para_grafo(self):
+        grafo = {}
+        def percorrer(no, pai):
+            if no is None:
+                return
+            grafo[str(no.valor)] = []
+            if pai is not None:
+                grafo[str(no.valor)].append(str(pai.valor))
+            if no.esquerda:
+                grafo[str(no.valor)].append(str(no.esquerda.valor))
+            if no.direita:
+                grafo[str(no.valor)].append(str(no.direita.valor))
+
+            percorrer(no.esquerda, no)
+            percorrer(no.direita, no)
+        percorrer(self.raiz, None)
+        return grafo
+            
+    
+    def preOrdem(self, no):
+        if no is not None:
+            print(no.valor)
+            self.preOrdem(no.esquerda)
+            self.preOrdem(no.direita)
+
+    def emOrdem(self, no):
+        if no is not None:
+            self.emOrdem(no.esquerda)
+            print(no.valor)
+            self.emOrdem(no.direita)
+    
+    def posOrdem(self, no):
+        if no is not None:
+            self.posOrdem(no.esquerda)
+            self.posOrdem(no.direita)
+            print(no.valor)
+arvore = Tree()
+valores = random.sample(range(1, 100), 20)
+
+print(f'Valores presentes na árvore: {valores}')
+for v in valores:
+    arvore.inserir(v)
+
+#Teste de arvore
+print('\nPré-Ordem: ', end=' '); arvore.preOrdem(arvore.raiz)
+print('\nEm-Ordem: ', end=' '); arvore.emOrdem(arvore.raiz)
+print('\nPós-Ordem: ', end=' '); arvore.posOrdem(arvore.raiz)
+print(f'\nAltura da Árvore: {arvore.altura(arvore.raiz)}')
+
+
+
+if __name__ == '__main__':
+    arvore = Tree()
+    valores = random.sample(range(1, 100), 20)
+    for v in valores:
+        arvore.inserir(v)
+    print(f'Valores: {valores}')
