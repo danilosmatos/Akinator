df8f1722861de4f8884dbea1ee8907d68094efa1
Danilo <danilosoaresmatos@gmail.com>
2026-03-17 08:36:34 -0300

    Reorganização Nó para o arquivo apropriado

--- Algorithms/node.py
+++ Algorithms/node.py
@@ -1,1 +1,7 @@
-﻿
+﻿class Node:
+    def __init__(self, pergunta = None, resposta = None):
+        self.pergunta = pergunta
+        self.resposta = resposta
+
+        self.yes = None
+        self.no = None
--- Algorithms/tree.py
+++ Algorithms/tree.py
@@ -1,11 +1,4 @@
-﻿class Node:
-    def __init__(self, pergunta = None, resposta = None):
-        self.pergunta = pergunta
-        self.resposta = resposta
-
-        self.yes = None
-        self.no = None
-
+﻿from node import Node
 class ArvoreDecisao:
 
     def __init__(self):
